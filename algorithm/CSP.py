eed7d83bce8026bfc09ac307b5a83d877327ec4a
Kshitij Goyal <117494616+KshitijGoyal2022@users.noreply.github.com>
2024-10-16 21:05:34 -0700

    small fix

--- algorithm/CSP.py
+++ algorithm/CSP.py
@@ -17,3 +17,6 @@ class CSP:
         for X, removed_values in removals.items():
             if X in self.domains:
                 self.domains[X].update(removed_values)
+    def conflicts(self, state1, color1, state2, color2):
+        # Check if two states are neighbors and have the same color
+        return color1 == color2 and state2 in self.adjList[state1]
